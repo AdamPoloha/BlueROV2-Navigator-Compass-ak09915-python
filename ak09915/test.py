1e150d6deedf0346661820404346ab3be1804e55
AdamPoloha <96154207+AdamPoloha@users.noreply.github.com>
2024-09-23 13:41:15 +1200

    Simple test with print instead of log

--- ak09915/test.py
+++ ak09915/test.py
@@ -2,22 +2,18 @@
 
 def main():
     from ak09915 import AK09915
-    from llog import LLogWriter
 
     device = "ak09915"
-    parser = LLogWriter.create_default_parser(__file__, device)
-    parser.add_argument("--bus", type=int, default=1, help="i2c bus")
-    args = parser.parse_args()
 
+    ak = AK09915()
 
-    with LLogWriter(args.meta, args.output, console=args.console) as log:
-        ak = AK09915(args.bus)
+    def data_getter():
+        data = ak.measure()
+        #print(f"{data.x_raw} {data.y_raw} {data.z_raw} {data.x:.6f} {data.y:.6f} {data.z:.6f}")
+        print(data.x_raw,data.y_raw,data.z_raw,data.x,data.y,data.z)
 
-        def data_getter():
-            data = ak.measure()
-            return f"{data.x_raw} {data.y_raw} {data.z_raw} {data.x:.6f} {data.y:.6f} {data.z:.6f}"
-
-        log.log_data_loop(data_getter, parser_args=args)
+    while True:
+        data_getter()
 
 if __name__ == '__main__':
     main()
